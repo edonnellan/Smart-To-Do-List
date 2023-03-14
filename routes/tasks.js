20a44fcdd30e428e935ea6a6432e85932066b465
SooyeonCheon123 <suyeon5445@gmail.com>
2023-03-14 16:52:15 -0700

    add google api

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,71 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
-// const request = require("request");
-
-const categoryFinder = function(taskTitle) {
-  const taskName = taskTitle.toLowerCase();
-  if (taskName.includes('buy') ||
-    taskName.includes('groceries') ||
-    taskName.includes('amazon') ||
-    taskName.includes('order') ||
-    taskName.includes('purchase')
-  ) {
-    return 'products';
-  } else if (
-    taskName.includes('watch') ||
-    taskName.includes('stream') ||
-    taskName.includes('movie') ||
-    taskName.includes('film') ||
-    taskName.includes('TV')
-  ) {
-    return 'movies_tv';
-  } else if (
-    taskName.includes('read') ||
-    taskName.includes('book')
-  ) {
-    return 'books';
-  } else if (
-    taskName.includes('eat') ||
-    taskName.includes('food')
-  ) {
-    return 'food';
-  } else {
-    return 'uncategorized';
-  }
- };
-
-const categoryFinder = function(taskTitle) {
-  const taskName = taskTitle.toLowerCase();
-  if (taskName.includes('buy') ||
-    taskName.includes('groceries') ||
-    taskName.includes('amazon') ||
-    taskName.includes('order') ||
-    taskName.includes('purchase')
-  ) {
-    return 'products';
-  } else if (
-    taskName.includes('watch') ||
-    taskName.includes('stream') ||
-    taskName.includes('movie') ||
-    taskName.includes('film') ||
-    taskName.includes('TV')
-  ) {
-    return 'movies_tv';
-  } else if (
-    taskName.includes('read') ||
-    taskName.includes('book')
-  ) {
-    return 'books';
-  } else if (
-    taskName.includes('eat') ||
-    taskName.includes('food')
-  ) {
-    return 'food';
-  } else {
-    return 'uncategorized';
-  }
- };
+const request = require("request");
 
 
 const categoryFinder = function(taskTitle) {
@@ -96,27 +32,40 @@ const categoryFinder = function(taskTitle) {
   ) {
     return 'food';
   } else {
-    categoryFinderApi(taskTitle);
+    let result =categoryFinderApi(taskTitle);
+    console.log("result", result);
+    return result;
   }
  };
 
  const categoryFinderApi = function(taskTitle) {
-  console.log("api call");
   request(`https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyAfocBMJa0FOqZWCAZWqOIjoF9BsU_BKyo&cx=c2c907cd368fa4916&q=${taskTitle}`, (err, response, body) => {
     const results = JSON.parse(body).items;
-    console.log(results);
+    // console.log(results);
+    const data = [];
+      for (const result of results) {
+        data.push(result.snippet);
+      }
+    console.log(data);
+
+    for (let element of data) {
+      console.log(element);
+      if (element.includes('novel')) {
+        console.log('book!');
+        const output = 'books';
+        return output;
+      }
+    }
   })
  };
 
 const addTask = function(title, category, priority) {
 
   if (category == 'uncategorized') {
+  console.log("category:", category)
     category = categoryFinder(title)
+    console.log("category:", category)
   };
-
-  if (category === 'uncategorized') {
-    category = categoryFinder(title);
-  }
 
   const taskValues = [
     `${title}`,
